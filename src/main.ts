7c7fdc5acb4caea5b19be9c3a76c2697279de9fa
Catatomik <cata@catadev.org>
2023-02-19 15:51:47 +0100

    ✨

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { getModelForClass, prop } from "@typegoose/typegoose";
+import { DocumentType } from "@typegoose/typegoose/lib/types";
+import { Types } from "mongoose"
+
+class Animal {
+
+  @prop({ required: true })
+  public species!: string;
+
+}
+const AnimalModel = getModelForClass(Animal);
+/** Here, type of `_id` isn't {@link Types.ObjectId}. */
+type AnimalDocument = DocumentType<Animal>;
+
+(async () => {
+
+  /** Type if "_id" is `any`, but should be {@link Types.ObjectId}. */
+  const animalId = (await AnimalModel.findOne({ species: "DoesThe'_id'TypeWork?" }));
+  animalId?._id
+
+})()
